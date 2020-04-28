7e07c6d7d138a91a323aecc59b19098bbcc7481b
mmuniz75 <mmunizs1975@gmail.com>
2020-04-28 10:50:37 -0300

    buscando usuario da api

--- src/app/users/user-list/user-list.component.ts
+++ src/app/users/user-list/user-list.component.ts
@@ -1,15 +1,30 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
+import { UserService } from '../user.service';
+import { Subject, Subscription } from 'rxjs';
+import { User } from '../user.model';
 
 @Component({
   selector: 'app-user-list',
   templateUrl: './user-list.component.html',
   styleUrls: ['./user-list.component.css']
 })
-export class UserListComponent implements OnInit {
+export class UserListComponent implements OnInit , OnDestroy{
 
-  constructor() { }
+  users : User[];
+  private userListener:Subscription;
+
+  constructor(private service:UserService) { }
 
   ngOnInit(): void {
+    this.userListener = this.service.usersListener.subscribe( 
+        usersList => this.users = usersList
+    );
+
+    this.service.fechtUsers();
   }
 
+  ngOnDestroy(): void {
+    this.userListener.unsubscribe();
+  }
+ 
 }
---
+++ src/app/users/user.service.ts
@@ -0,0 +1,27 @@
+import { Injectable } from '@angular/core';
+
+import { HttpClient } from '@angular/common/http';
+import { environment } from 'src/environments/environment';
+import { User } from './user.model';
+import { Subject } from 'rxjs';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class UserService {
+
+  usersListener = new Subject<User[]>();
+  users: User[];
+
+  constructor(private http: HttpClient) { }
+
+  fechtUsers() {
+    this.http.get<User[]>(`${environment.SERVER_URL}/users`)
+      .subscribe(response => {
+        this.users = response;
+        this.usersListener.next(this.users);
+      }
+      );
+  }
+
+}
